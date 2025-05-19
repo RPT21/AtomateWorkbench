544b3ed19d2f6fcf0f50660522cbf98d7b1dcd88
RPT21 <marremiau@outlook.com>
2025-05-19 21:39:20 +0200

    First complete revision of update plugin

--- plugins/update/update/__init__.py
+++ plugins/update/update/__init__.py
@@ -3,12 +3,13 @@
 # Decompiled from: Python 3.12.2 (tags/v3.12.2:6abddd9, Feb  6 2024, 21:26:36) [MSC v.1937 64 bit (AMD64)]
 # Embedded file name: ../plugins/update/src/update/__init__.py
 # Compiled at: 2004-08-02 04:12:16
-import kernel.plugin
+import lib.kernel.plugin, lib.kernel as kernel
 
 class UpdatePlugin(kernel.plugin.Plugin):
     __module__ = __name__
 
     def __init__(self):
+        super().__init__()
         self.contextBundle = None
         return
 
